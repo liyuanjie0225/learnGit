8573d56ea8dc2fe5934288dcbce030d6dc540f5f
yuanjie <834230276@qq.com>
2018-02-03 11:47:10 +0900

    vue学习第一天

---
+++ routerLearn/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent }          from './app.component';
+
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HeroesModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    CrisisListComponent,
+    NotFoundComponent
+],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
--- test.js
+++
@@ -1,6 +0,0 @@
-function(){
-	
-console.log('change  file');
-console.log('在来一次')	
-console.log('暂存两下')
-}
